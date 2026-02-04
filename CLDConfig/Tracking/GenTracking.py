bcb59bb4a844a3f84dd5998e2f8acc4142fa35c2
doloresgarcia <doloresg382@gmail.com>
2026-02-04 16:04:00 +0100

    back to non-tracking volume and added gen tracking

---
+++ CLDConfig/Tracking/GenTracking.py
@@ -0,0 +1,47 @@
+#
+# Copyright (c) 2014-2024 Key4hep-Project.
+#
+# This file is part of Key4hep.
+# See https://key4hep.github.io/key4hep-doc/ for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from Gaudi.Configuration import WARNING
+from Configurables import MarlinProcessorWrapper
+from Configurables import TracksFromGenParticles
+from Gaudi.Configuration import *
+
+tracksFromGenParticles = TracksFromGenParticles("CreateTracksFromGenParticles",
+                                                    InputGenParticles=["MCParticles"],
+                                                    InputSimTrackerHits=["VertexBarrelCollection",
+                                                                         "InnerTrackerBarrelCollection",
+                                                                         "OuterTrackerBarrelCollection",
+                                                                         "VertexEndcapCollection",
+                                                                         "InnerTrackerEndcapCollection", 
+                                                                         "OuterTrackerEndcapCollection"],
+                                                    OutputTracks=["SiTracks_Refitted"],
+                                                    OutputMCRecoTrackParticleAssociation=["SiTracks_Refitted_Relation"],
+                                                    ExtrapolateToECal=True,
+                                                    KeepOnlyBestExtrapolation=False,
+                                                    TrackerIDs=[1,
+                                                                3,
+                                                                5,
+                                                                2,
+                                                                4,
+                                                                6],
+                                                    # OutputLevel=DEBUG
+                                    )
+
+GenTrackingSequence = [
+    tracksFromGenParticles,
+]
